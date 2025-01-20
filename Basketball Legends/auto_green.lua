d477ccd3c8a2eab783a51cf4b7e704529819da0e
afy <73753274+afyzone@users.noreply.github.com>
2025-01-20 05:17:36 -0800

    Create auto_green.lua

---
+++ Basketball Legends/auto_green.lua
@@ -0,0 +1,11 @@
+local players = game:GetService('Players')
+local client = players.LocalPlayer
+local playergui = client:WaitForChild('PlayerGui')
+
+playergui.Visual.Shooting:GetPropertyChangedSignal('Visible'):Connect(function()
+    if (playergui.Visual.Shooting.Visible) then
+        task.delay(0.3, function()
+            game:GetService("ReplicatedStorage").Packages.Knit.Services.ControlService.RE.Shoot:FireServer(100)
+        end)
+    end
+end)
