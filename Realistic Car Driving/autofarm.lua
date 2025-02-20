10f2a08b762488aa434a4aa03a21bf5d19f95b6d
afy <73753274+afyzone@users.noreply.github.com>
2025-02-20 12:29:27 -0800

    Update autofarm.lua

--- Realistic Car Driving/autofarm.lua
+++ Realistic Car Driving/autofarm.lua
@@ -1,18 +1,31 @@
 -- https://www.roblox.com/games/5829141886
 local players = game:GetService('Players')
 local client = players.LocalPlayer
+local last_wheelset_time = 0
 
 shared.afy = not shared.afy
 
 local set_car = function(car, pos)
     for i,v in pairs(car:GetDescendants()) do
-        if (not v:IsA('BasePart') or v:IsDescendantOf(car.Wheels)) then continue end
+        if (not v:IsA('BasePart')) then continue end
+        
+        v.CFrame = CFrame.new(v.CFrame.X, pos.Y, v.CFrame.Z)
+        
+        if (v:IsDescendantOf(car.Wheels)) then 
+            if (os.clock() - last_wheelset_time > 0.2) then
+            	v.CFrame = pos
+            	last_wheelset_time = os.clock()
+            end
+            
+            continue 
+        end
 
         v.CanCollide = false
         v.CFrame = pos
     end
 end
 
+print(shared.afy)
 while (shared.afy) do task.wait()
     local my_car = workspace:FindFirstChild(client.Name .. 'sCar')
 
