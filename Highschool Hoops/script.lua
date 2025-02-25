a27b77cd6c1a31ef0ce577eace3648048d81b2dc
afy <73753274+afyzone@users.noreply.github.com>
2025-02-24 16:32:05 -0800

    Create script.lua

---
+++ Highschool Hoops/script.lua
@@ -0,0 +1,182 @@
+--https://www.roblox.com/games/13876564679
+-- Auto Guard, Auto Green
+
+local services = setmetatable({}, {
+    __index = function(self, key)
+        local service = pcall(cloneref, game:FindService(key)) and cloneref(game:GetService(key)) or Instance.new(key)
+        rawset(self, key, service)
+
+        return rawget(self, key)
+    end
+})
+
+local players = services.Players
+local runservice = services.RunService
+local virtualinputmanager = services.VirtualInputManager
+
+local client = players.LocalPlayer
+local playergui = client:WaitForChild('PlayerGui')
+local random = Random.new()
+local firing = false
+local target_position, body_velocity
+
+local hoops = {}; do
+    for i,v in (workspace.Hoops:GetDescendants()) do
+        if (not v.Name:find('Hoop')) then continue end
+
+        table.insert(hoops, v.Rim.Rim)
+    end
+end
+
+local get_char, get_root, get_hum, setup_shoot, position_between_two_instances, get_closest_in_table; do
+    get_char = function(player)
+        return player.Character
+    end
+
+    get_root = function(char)
+        return char and char:FindFirstChild('HumanoidRootPart')
+    end
+
+    get_hum = function(char)
+        return char and char:FindFirstChildWhichIsA('Humanoid')
+    end
+
+    setup_shoot = function()
+        virtualinputmanager:SendKeyEvent(false, 'E', false, nil)
+
+        if (connection) then
+            connection:Disconnect()
+            connection = nil
+        end
+    end
+
+    position_between_two_instances = function(instance, instance2, distance)
+        local pivot_pos, pivot_pos2 = instance:GetPivot().Position, instance2:GetPivot().Position
+        local magnitude = vector.magnitude(pivot_pos - pivot_pos2)
+
+        return (pivot_pos):Lerp(pivot_pos2, distance / magnitude)
+    end
+
+    get_closest_in_table = function(tbl, range)
+        local char = get_char(client)
+        local root = get_root(char)
+        local dist, closest = math.huge
+
+        if not (char and root) then return end
+
+        for i,v in (tbl) do
+            local mag = vector.magnitude(v:GetPivot().Position - root.Position)
+
+            if (range and mag > range) then continue end
+
+            if (mag < dist) then
+                closest = v
+                dist = mag
+            end
+        end
+
+        return closest, dist
+    end
+end
+
+local con; con = runservice.Heartbeat:Connect(function()
+    if (not shared.afy) then
+        con:Disconnect()
+    end
+
+    local char = get_char(client)
+    local root = get_root(char)
+    local hum = get_hum(char)
+
+    if not (char and root and hum) then return end
+    
+    if (not body_velocity) then
+        body_velocity = Instance.new("BodyVelocity")
+        body_velocity.Parent = root
+        body_velocity.MaxForce = Vector3.new(math.huge, 0, math.huge)
+    end
+    
+    local guarding = char:GetAttribute('Guarding')
+
+    if (guarding and target_position) then
+        local currentPos = root.Position
+        local direction_xz = vector.create(target_position.X - currentPos.X, 0, target_position.Z - currentPos.Z)
+
+        if direction_xz.magnitude < 2 then
+            body_velocity.Velocity = vector.zero
+            body_velocity:Destroy()
+            body_velocity = nil
+            target_position = nil
+        else
+            body_velocity.Velocity = direction_xz.Unit * hum.WalkSpeed
+        end
+    else
+        if (body_velocity) then
+            body_velocity:Destroy()
+            body_velocity = nil
+        end
+    end
+end)
+
+shared.afy = not shared.afy
+
+print(shared.afy)
+while (shared.afy and task.wait()) do
+    local char = get_char(client)
+    local root = get_root(char)
+    local hum = get_hum(char)
+
+    if (char and root and hum) then
+        local meter = playergui:FindFirstChild('ShotMeter')
+
+        if (meter) then
+            if (meter.Enabled) then
+                if (not firing) then
+                    firing = true
+
+                    task.spawn(function()
+                        while (meter.Enabled and meter.Base.Bar.UIGradient.Offset.Y > 0.2) do task.wait() end
+
+                        virtualinputmanager:SendKeyEvent(false, 'E', false, nil)
+                    end)
+                end
+            else
+                firing = false
+            end
+        end
+
+        local guarding = char:GetAttribute('Guarding')
+
+        if (guarding) then
+            local balls = {}; do
+                for i,v in (workspace.Balls:GetChildren()) do
+                    local ball_owner_name = v:GetAttribute('CurrentOwner')
+
+                    if (ball_owner_name and ball_owner_name ~= '') then 
+                        local ball_owner = players:FindFirstChild(ball_owner_name)
+                        local ball_owner_char = ball_owner and get_char(ball_owner)
+
+                        if (ball_owner_char) then
+                            table.insert(balls, ball_owner_char)
+                        end
+                    end
+                end
+            end
+
+            local closest_ball_holder = get_closest_in_table(balls, 15)
+            local closest_hoop = get_closest_in_table(hoops)
+
+            if (closest_ball_holder and closest_hoop) then
+                local move_pos = position_between_two_instances(closest_ball_holder, closest_hoop, closest_ball_holder.Head.Position.Y > (char.Head.Position.Y + 1) and random:NextInteger(1.8, 2.2) or random:NextInteger(6.8, 7.2))
+
+                if (move_pos) then
+                    local direction = move_pos - root.Position
+
+                    if (vector.magnitude(direction) > 0.2) then
+                        target_position = move_pos
+                    end
+                end
+            end
+        end
+    end
+end
