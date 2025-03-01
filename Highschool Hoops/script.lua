70def6719a8c3c2804a9424d8bdf8ea004f75480
afy <73753274+afyzone@users.noreply.github.com>
2025-02-28 16:27:20 -0800

    Update script.lua

--- Highschool Hoops/script.lua
+++ Highschool Hoops/script.lua
@@ -199,8 +199,12 @@ local input_ended_con; input_ended_con = userinputservice.InputEnded:Connect(fun
     end
 end)
 
-local on_teleport = client.OnTeleport:Connect(function()
-    if (queue_on_teleport and shared.afy) then
+local on_teleport; on_teleport = client.OnTeleport:Connect(function()
+    if (not shared.afy) then
+        on_teleport:Disonnect()
+    end
+
+    if (queue_on_teleport) then
         queue_on_teleport(`loadstring(game:HttpGet('https://raw.githubusercontent.com/afyzone/lua/refs/heads/main/Highschool%20Hoops/script.lua'))()`)
     end
 end)
