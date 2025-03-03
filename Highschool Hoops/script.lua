90d1c7af45b644c25ccee18812d54065166f6f5e
afy <73753274+afyzone@users.noreply.github.com>
2025-03-03 00:29:48 -0800

    Update script.lua

--- Highschool Hoops/script.lua
+++ Highschool Hoops/script.lua
@@ -18,6 +18,9 @@ local runservice = services.RunService
 local virtualinputmanager = services.VirtualInputManager
 local userinputservice = services.UserInputService
 
+if (not afy_executed) then return end
+getgenv().afy_executed = true
+
 local client = players.LocalPlayer
 local playergui = client:WaitForChild('PlayerGui')
 local random = Random.new()
