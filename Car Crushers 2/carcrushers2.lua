c23abef069f55d2583b2400b4a9d7f846bc546f9
afy <73753274+afyzone@users.noreply.github.com>
2025-03-12 17:23:45 -0700

    Update carcrushers2.lua

--- Car Crushers 2/carcrushers2.lua
+++ Car Crushers 2/carcrushers2.lua
@@ -1,16 +1,19 @@
--- Services
 local players = game:GetService("Players")
 local replicatestorage = game:GetService("ReplicatedStorage")
 local runservice = game:GetService("RunService")
 
--- Variables
 local client = players.LocalPlayer
 local car_collection = workspace.CarCollection
 local gui_script = getsenv(client.PlayerGui:FindFirstChild("GUIs"))
 local open_func = gui_script["OpenDealership"]
 local spawn_func = gui_script["SpawnButton"]
 
--- Functions
+local flags = {
+    initial_spawn = 0,
+    spawn_time = 0,
+    last_flip = false,
+}
+
 local get_current_car, get_char, get_root, get_hum, retrieve_money, retrieve_best_car; do
     get_current_car = function()
         local car = car_collection:FindFirstChild(client.Name)
@@ -47,45 +50,36 @@ local get_current_car, get_char, get_root, get_hum, retrieve_money, retrieve_bes
     retrieve_best_car = function()
         open_func()
         spawn_func(true, Enum.UserInputState.Begin)
+        flags.initial_spawn = os.clock()
     end
 
-    destroy_car = function()
-        local char = get_char(client)
-        local hum = get_hum(char)
-        local root = get_root(char)
-
-        local initial_spawn, spawn_time = os.clock(), os.clock()
-
-        if (char and hum and root and get_current_car()) then
-            while (os.clock() - initial_spawn < 30 and task.wait()) do
-                local car = get_current_car()
-                
-                if (not car) then break end
-
-                if (car and os.clock() - spawn_time > 0.25) then
-                    spawn_time = os.clock()
-                    last_flip = not last_flip
-
-                    car.PrimaryPart.Velocity = vector.create(0, 250 * (not last_flip and 1 or -1), 0)
-                    car.PrimaryPart.CFrame *= CFrame.Angles(180, 0, 0)
-                end
-            end
-            
+    destroy_car = function(car)
+        if (os.clock() - flags.initial_spawn >= 30) then
             replicatestorage.rE.Delete:FireServer()
+        end
+
+        if (car and os.clock() - flags.spawn_time > 0.25) then
+            flags.spawn_time = os.clock()
+            flags.last_flip = not flags.last_flip
+
+            car.PrimaryPart.Velocity = vector.create(0, 250 * (not flags.last_flip and 1 or -1), 0)
+            car.PrimaryPart.CFrame *= CFrame.Angles(180, 0, 0)
         end
     end
 end
 
--- Loop
 shared.afy = not shared.afy
 
 print(shared.afy)
 while (shared.afy and task.wait()) do
     local char = get_char(client)
     local able_car = spawn_check()
+    local current_car = get_current_car()
 
-    if (char and able_car) then
+    if (current_car) then
+        destroy_car(current_car)
+
+    elseif (char and able_car) then
         retrieve_best_car()
-        destroy_car()
     end
 end
