768d48ca324aa7b8faaf6decea73e9921e8c5e23
afy <73753274+afyzone@users.noreply.github.com>
2025-03-22 06:19:43 -0700

    Update MoneyFarm.lua

--- BlockSpin/MoneyFarm.lua
+++ BlockSpin/MoneyFarm.lua
@@ -3,4 +3,4 @@
 -- For Blockspin Developers
 -- If you guys want tips on how to make an anti cheat. DM me @afyzone
 
-loadstring(game:HttpGet('https://gist.githubusercontent.com/afyzone/f6b069c544830c1f9131ba0e501730cc/raw/8dfbc5e499a48228bf7f0651e8512b08c870283f/blockspin_autofarm.lua'))()
+loadstring(game:HttpGet('https://gist.githubusercontent.com/afyzone/f6b069c544830c1f9131ba0e501730cc/raw/'))()
