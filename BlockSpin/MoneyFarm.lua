fd0ec98fbea675b70a4f78d3d6f3cdaf4e40ac21
afy <73753274+afyzone@users.noreply.github.com>
2025-03-24 05:04:17 -0700

    Update MoneyFarm.lua

--- BlockSpin/MoneyFarm.lua
+++ BlockSpin/MoneyFarm.lua
@@ -3,4 +3,11 @@
 -- For Blockspin Developers
 -- If you guys want tips on how to make an anti cheat. DM me @afyzone
 
+local Players = game:GetService('Players')
+local Client = Players.LocalPlayer
+
+for i,v in getconnections(Client.Idled) do
+	v:Disable()
+end
+
 loadstring(game:HttpGet('https://gist.githubusercontent.com/afyzone/f6b069c544830c1f9131ba0e501730cc/raw/'))()
