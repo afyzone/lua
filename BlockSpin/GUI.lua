ab5fc13530d6c534aeff3993379df2d0790a96f0
afy <73753274+afyzone@users.noreply.github.com>
2025-03-26 20:43:36 -0700

    Update GUI.lua

--- BlockSpin/GUI.lua
+++ BlockSpin/GUI.lua
@@ -1,3 +1,3 @@
 -- hella features
 
-loadstring(game:HttpGet('https://gist.githubusercontent.com/afyzone/63b770a916e780efb3c42a93780a2551/raw/'))()
+loadstring(game:HttpGet('https://gist.githubusercontent.com/afyzone/6e5c4d36b29f38570aaf87ea94e4f445/raw/'))()
