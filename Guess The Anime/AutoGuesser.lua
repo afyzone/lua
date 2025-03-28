ec7607faed62e1b524554f1349d9cd82df49112f
afy <73753274+afyzone@users.noreply.github.com>
2025-03-28 07:50:30 -0700

    Update AutoGuesser.lua

--- Guess The Anime/AutoGuesser.lua
+++ Guess The Anime/AutoGuesser.lua
@@ -9,40 +9,40 @@ local GetChar, CharacterAdded, CreateConnection; do
 	GetChar = function(player)
 		return player and player.Character
 	end
-    
-    CharacterAdded = function(Char)
-        if (not Char) then return end
+	
+	CharacterAdded = function(Char)
+		if (not Char) then return end
 
-        for i,v in (Connections) do
-            v:Disconnect()
-            v = nil
-        end
+		for i,v in (Connections) do
+			v:Disconnect()
+			v = nil
+		end
 
-        for i,v in workspace.Quiz:GetDescendants() do
-            if (not v:IsA('TouchTransmitter')) then continue end
+		for i,v in workspace.Quiz:GetDescendants() do
+			if (not v:IsA('TouchTransmitter')) then continue end
 
-            local Part = v.Parent
-            
-            CreateConnection(Part.Touched, function(HitPart)
-                if (Part.Transparency ~= 1 and tick() - LastHit > 1 and HitPart:IsDescendantOf(Char)) then
-                    local Answers = Part.Script.Answers.Value:split('|')
+			local Part = v.Parent
+			
+			CreateConnection(Part.Touched, function(HitPart)
+				if (Part.Transparency ~= 1 and tick() - LastHit > 1 and HitPart:IsDescendantOf(Char)) then
+					local Answers = Part.Script.Answers.Value:split('|')
 
-                    local Random = math.random(#Answers)
-                    local Answer = Answers[Random]
+					local Random = math.random(#Answers)
+					local Answer = Answers[Random]
 
-                    TextChatService.TextChannels.RBXGeneral:SendAsync(Answer)
-                    LastHit = tick()
-                end
-            end)
-        end
-    end
+					TextChatService.TextChannels.RBXGeneral:SendAsync(Answer)
+					LastHit = tick()
+				end
+			end)
+		end
+	end
 
-    CreateConnection = function(signal, func)
-        local Conn = signal:Connect(func)
-        table.insert(Connections, Conn)
+	CreateConnection = function(signal, func)
+		local Conn = signal:Connect(func)
+		table.insert(Connections, Conn)
 
-        return Conn
-    end
+		return Conn
+	end
 end
 
 CharacterAdded(Client.Character)
@@ -54,6 +54,6 @@ print(shared.afy)
 while (shared.afy and task.wait()) do end
 
 for i,v in (Connections) do
-    v:Disconnect()
-    v = nil
+	v:Disconnect()
+	v = nil
 end
