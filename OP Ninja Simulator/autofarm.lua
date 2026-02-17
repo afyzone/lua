71d752bd3e3e6d54e4c7f8d0c7b1831da1eb5851
afy <73753274+afyzone@users.noreply.github.com>
2026-02-17 01:57:57 -0500

    Update autofarm.lua

--- OP Ninja Simulator/autofarm.lua
+++ OP Ninja Simulator/autofarm.lua
@@ -1,138 +1,113 @@
 -- https://www.roblox.com/games/4225025295 | Exec twice to toggle
 
-local players = game:GetService('Players')
-local virtualinputmanager = game:GetService('VirtualInputManager')
-local guiservice = game:GetService("GuiService")
-local client = players.LocalPlayer
-local backpack = client:FindFirstChildWhichIsA('Backpack')
-local playergui = client:WaitForChild('PlayerGui')
-local gui_inset = guiservice:GetGuiInset()
-
-local backpack_filter, safe_pos = {"Shuriken", "InvisibilityTool", "ShadowCloneTool", "TeleportTool"}
-
 shared.afy = not shared.afy
+print('[afy]', shared.afy)
 
-for i,v in getconnections(client.Idled) do
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local OriginalRequire = require
+local GameRequire = (function()
+    require(ReplicatedStorage:FindFirstChild("Require") or game:FindFirstChild("Require", true))()
+    return require
+end)()
+require = OriginalRequire
+
+local Players = game:GetService('Players')
+local Client = Players.LocalPlayer
+local PlayerGui = Client:WaitForChild('PlayerGui')
+local Backpack = Client:FindFirstChildWhichIsA('Backpack')
+local BackpackFilter, SafePos = {"Shuriken", "InvisibilityTool", "ShadowCloneTool", "TeleportTool"}
+local SwordDelay, ShurikenDelay, ClassDelay, RealmDelay = 0, 0, 0, 0
+local PersonalRemotes = GameRequire('PersonalRemoteService')
+local RemoteData = {
+    Sword = 'อัพเกรดดาบแม็กซ์',
+    Shuriken = "อัพเกรดดาวกระจาย",
+    Realm = "อัพเกรดแอสเซนต์",
+    Class = "อัพเกรดคลาส"
+}
+
+for i,v in getconnections(Client.Idled) do
     v:Disable()
 end
 
-if (not safe_part) then
-    local safe_part, random_coordinate = Instance.new('Part'), math.random(3500); do
-        getgenv().safe_part = safe_part
+if not SafePart then
+    local SafePart, RandomCoordinate = Instance.new('Part'), math.random(4e3); do
+        getgenv().SafePart = SafePart
 
-        safe_part.Parent = workspace
-        safe_part.Anchored = true
-        safe_part.Size = Vector3.new(150,1,150)
-        safe_part.CFrame = CFrame.new(Vector3.new(9999,10000,9999) + Vector3.new(random_coordinate))
+        SafePart.Parent = workspace
+        SafePart.Anchored = true
+        SafePart.Size = vector.create(150, 1, 150)
+        SafePart.CFrame = CFrame.new(vector.create(10e3, 10e3, 10e3) + vector.create(RandomCoordinate, 0, 0))
     end
 end
 
-local click_button = function(button)
-    virtualinputmanager:SendMouseButtonEvent(button.AbsolutePosition.X + button.AbsoluteSize.X / 2, button.AbsolutePosition.Y + gui_inset.Y + (button.AbsoluteSize.Y / 2), 0, true, button, 1);
-    virtualinputmanager:SendMouseButtonEvent(button.AbsolutePosition.X + button.AbsoluteSize.X / 2, button.AbsolutePosition.Y + gui_inset.Y + (button.AbsoluteSize.Y / 2), 0, false, button, 1)
-end
+local AutoUpgrade = function()
+    local UpgradeF = PlayerGui:WaitForChild('MainGui'):FindFirstChild("UpgradeF")
 
-local sword_delay, shuriken_delay, class_delay, realm_delay = 0, 0, 0, 0
-local auto_upgrade = function()
-    local main_gui = playergui:WaitForChild('MainGui'):FindFirstChild("UpgradeF")
-    local failed_upgrade = playergui:WaitForChild('MainGui'):FindFirstChild("UpgradeFailedF")
-    local failed_class = playergui:WaitForChild('MainGui'):FindFirstChild("UpgradeAscensionFailedF")
-    local failed_ascension = playergui:WaitForChild('MainGui'):FindFirstChild("UpgradeClassFailedF")
+    if UpgradeF then
+        local Sword = UpgradeF["SwordF"]:FindFirstChild("MaxUpgradeBtn")
+        local Shuriken = UpgradeF["ShurikenF"]:FindFirstChild("ShurikenImgBtn")
+        local Class = UpgradeF["ClassF"]:FindFirstChild("ClassImgBtn")
+        local Realm = UpgradeF["AscendF"]:FindFirstChild("AscendImgBtn")
 
-    if (failed_upgrade and failed_upgrade.Visible) then
-        failed_upgrade.Visible = false
-    end
-
-    if (failed_class and failed_class.Visible) then
-        failed_class.Visible = false
-    end
-
-    if (failed_ascension and failed_ascension.Visible) then
-        failed_ascension.Visible = false
-    end
-
-    if (main_gui) then
-        main_gui.Position = UDim2.fromScale(0.5, 0.5)
-        
-        local sword = main_gui["SwordF"]:FindFirstChild("MaxUpgradeBtn")
-        local shuriken = main_gui["ShurikenF"]:FindFirstChild("ShurikenImgBtn")
-        local class = main_gui["ClassF"]:FindFirstChild("ClassImgBtn")
-        local realm = main_gui["AscendF"]:FindFirstChild("AscendImgBtn")
-
-        if (sword and tick() - sword_delay > 5) then
-            task.wait()
-            click_button(sword)
-
-            sword_delay = tick()
+        if Sword and tick() - SwordDelay > 2 then
+            PersonalRemotes.RemoteFunction:InvokeServer(RemoteData.Sword)
+            SwordDelay = tick()
         end
 
-        if (shuriken and tick() - shuriken_delay > 5) then
-            task.wait()
-            click_button(shuriken)
-
-            shuriken_delay = tick()
+        if Shuriken and tick() - ShurikenDelay > 2 then
+            PersonalRemotes.RemoteFunction:InvokeServer(RemoteData.Shuriken)
+            ShurikenDelay = tick()
         end
 
-        if (class and tick() - class_delay > 5) then
-            task.wait()
-            click_button(class)
-            
-            class_delay = tick()
+        if Class and tick() - ClassDelay > 2 then
+            PersonalRemotes.RemoteFunction:InvokeServer(RemoteData.Class)
+            ClassDelay = tick()
         end
 
-        if (realm and tick() - realm_delay > 5) then
-            task.wait()
-            click_button(realm)
-            
-            realm_delay = tick()
+        if Realm and tick() - RealmDelay > 2 then
+            PersonalRemotes.RemoteFunction:InvokeServer(RemoteData.Realm)
+            RealmDelay = tick()
         end
     end
 end
 
-while (shared.afy and task.wait()) do
-    local char = client.Character
+while shared.afy and task.wait() do
+    local Char = Client.Character
+    local Root = Char and Char:FindFirstChild('HumanoidRootPart')
 
-    if (char) then
-        local root = char:FindFirstChild('HumanoidRootPart')
+    if Root then
+        if not SafePos then
+            SafePos = Root.CFrame
+            Root.CFrame = SafePart.CFrame * CFrame.new(0, 3, 0)
+        end
 
-        if (root) then
-            if (not safe_pos) then
-                safe_pos = root.CFrame
-                root.CFrame = safe_part.CFrame * CFrame.new(0, 3, 0)
+        local Katana = (function()
+            for Index, Tool in (Backpack:GetChildren()) do
+                if table.find(BackpackFilter, Tool.Name) then continue end
+                return Tool
             end
-
-            local katana = (function()
-                for i,v in (backpack:GetChildren()) do
-                    if (table.find(backpack_filter, v.Name)) then continue end
-        
-                    return v
-                end
-            end)()
-        
-            if (katana) then
-                katana.Parent = char
-            end
-
-            katana = char:FindFirstChildWhichIsA('Tool')
-
-            if (katana) then
-                katana:Activate()
-                katana.Parent = backpack
-            end
-
-            auto_upgrade()
+        end)()
+    
+        if Katana then
+            Katana.Parent = Char
         end
+
+        local Katana = Char:FindFirstChildWhichIsA('Tool')
+
+        if Katana then
+            Katana:Activate()
+            Katana.Parent = Backpack
+        end
+
+        AutoUpgrade()
     end
 end
 
-if (safe_pos) then
-    local char = client.Character
+if SafePos then
+    local Char = Client.Character
+    local Root = Char and Char:FindFirstChild('HumanoidRootPart')
 
-    if (char) then
-        local root = char:FindFirstChild('HumanoidRootPart')
-
-        if (root) then
-            root.CFrame = safe_pos
-        end
+    if Root then
+        Root.CFrame = SafePos
     end
 end
